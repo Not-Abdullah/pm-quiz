cd1d99da98151cfd674a37689d4f4d34af0d23fe
5025690@learning.deakincollege.edu.au <5025690@learning.deakincollege.edu.au>
2024-11-14 03:13:26 +0000

    Created edmund_barton.png; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -8,3 +8,19 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def Alfred_deakin_btn_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    alert("Incorrect. Please try again.")
+
+  def Edmud_barton_btn_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    alert("Correct! Edmud Barton is the firs prime minister of Australia.")
+
+  def John_curtin_btn_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    alert("Incorrect. Please try again.")
+
+  def andrew_fisher_btn_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    alert("Incorrect. Please try again.")
